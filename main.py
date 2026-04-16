a72d0cf55545c02cf4110e4a9799761a3af4bddf
Lavanya Boda <lavanyaboda17@gmail.com>
2026-04-16 10:36:55 +0530

    added quick sort

--- main.py
+++ main.py
@@ -10,15 +10,28 @@ bubble_sort(arr) #call function
 print(f"After sorting: {arr}")
                 
 # Quick Sorting
-
-
+def quick_sort(arr): 
+    if len(arr) <= 1:      
+        return arr
+    pivot = arr[0]     #first item as pivot
+    smaller = [x for x in arr[1:] if x <= pivot]   
+    bigger  = [x for x in arr[1:] if x > pivot]    
+    return quick_sort(smaller) + [pivot] + quick_sort(bigger)
+arr = [5, 3, 8, 1, 9, 2]
+print(quick_sort(arr))  
 
 #palindrome
-word = input("Enter String: ")
-if word == word[::-1]:  #[::-1] reverse string == palindrome
-        print(word, "is a Palindrome")
+def palindrome(word):
+    if word == word[::-1]:
+        return True
+    return False
+text = input("Enter string: ")
+
+if palindrome(text):
+    print(text,"is a palindrome")
 else:
-    print(word, "is not a palindrome")
+    print(text,"is not a palindrome")
+        
     
     
 # Factorial
