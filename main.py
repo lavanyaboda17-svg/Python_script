7f02f2d6774226e38d91cf15a3b06ab4fa0cb147
Lavanya Boda <lavanyaboda17@gmail.com>
2026-04-20 12:45:48 +0530

    added four sum code

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ def bubble_sort(arr):
         for j in range(len(arr)-i-1):
             if arr[j] > arr[j+1]:
                 arr[j] , arr[j+1] = arr[j+1] , arr[j] #swap
-arr = list(map(int, input("Enter number: ").split(",")))
+arr = list(map(int, input("Enter number(bubble_sort): ").split(",")))
 print(f"Before sorting: {arr}")
 bubble_sort(arr) #call function
 print(f"After sorting: {arr}")
@@ -67,7 +67,7 @@ def two_sum(num, target):
     return "no pair found"
 
 
-num = list(map(int, input("Enter numbers: ").split()))
+num = list(map(int, input("Enter numbers(sum of two num): ").split()))
 target = int(input("Enter target: "))
 
 print(two_sum(num, target))
@@ -87,5 +87,27 @@ def threeSum(nums):
     return result
 nums = [-1, 0, 1, 2, -1, -4]
        #[0, 1, 2, 3,  4,  5]
-print(threeSum(nums))
-            
+print("three sum:",threeSum(nums))
+
+#sum of four numbers
+def fourSum(nums, target):
+    n = len(nums)
+    result = []
+    
+    for i in range(n):
+        for j in range(i + 1, n):
+            for k in range(j + 1, n):
+                for l in range(k + 1, n):
+                    if nums[i] + nums[j] + nums[k] + nums[l] == target:
+                        quad = [nums[i], nums[j], nums[k], nums[l]]
+                        if quad not in result:
+                            result.append(quad)
+    return result
+
+nums = [1, 0, -1, 0, -2, 2]
+target = 0
+num_1 = [2,4,5,8,2,4]
+target_1 = 12
+
+print("four_sum:",fourSum(nums, target))
+print("four_sum",fourSum(num_1, target_1))
