d6aa620a76a692af1d56b5a9436f9b93bb41c6ab
Lavanya Boda <lavanyaboda17@gmail.com>
2026-05-08 15:19:16 +0530

    removed files

--- class_ex.py
+++
@@ -1,9 +0,0 @@
-class student:
-    def __init__(self,name ,age):
-        self.name = name
-        self.age = age
-    s1 = ("neha",20)
-    s2 = ("lavanya",20)
-    print(s1)
-    print(s2)
-    
